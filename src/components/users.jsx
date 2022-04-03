849a63fef79644db258a6a92a7a8a732a0a463ba
Evgeny <estrelnikov1@yandex.ru>
2022-04-03 22:25:46 +0300

    task_2.3.2

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
-import api from "../api"
+import React from "react";
+import User from "./user";
 
-const Users = () => {
-    const [users, setUsers] = useState(api.users.fetchAll());
-    const handleDelete = (userId) => {
-        setUsers(prevState => prevState.filter((user) => user._id !== userId))
-    }
-    const renderPhrase = (number) => {
-        let arrNumber = String(number).split('');
-        if (number === 0) {
-            return (
-                <span className={`badge bg-danger`}>
-                    {'Никто с тобой не тусанет'}
-                </span>
-        )}
-        if (
-            (arrNumber.length >= 1 && 
-            arrNumber[arrNumber.length - 1] > 1 &&
-            arrNumber[arrNumber.length - 1] < 5 &&
-            arrNumber[arrNumber.length - 2] > 1) ||
-            (arrNumber.length === 1 &&
-            arrNumber[arrNumber.length - 1] > 1 &&
-            arrNumber[arrNumber.length - 1] < 5)
-            ) {
-            return (
-                <span className={`badge bg-primary`}>
-                    { number + ' человека тусанут с тобой сегодня'}
-                </span>
-        )} else {
-            return (
-                <span className={`badge bg-primary`}>
-                    {number + ' человек тусанет с тобой сегодня'}
-                </span>
-            )
-        }
-    }
+const Users = (props) => {
     return (
         <>
-            <h1>{renderPhrase(users.length)}</h1>
-            { users.length > 0 ?
+            { props.lenght > 0 ?
                 <table className="table">
                     <thead key="thead">
                         <tr>
@@ -47,27 +13,23 @@ const Users = () => {
                         <th scope="col">Профессия</th>
                         <th scope="col">Встретился, раз</th>
                         <th scope="col">Оценка</th>
+                        <th scope="col">Избранное</th>
                         <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody key="tbody">
-                        {users.map((user,index) => (
-                            <tr key={user._id}>
-                                <td>{user.name}</td>
-                                <td>{user.qualities.map(quality => (
-                                    <span className={`badge m-1 bg-${quality.color}`} key={quality._id}>{quality.name}</span>
-                                ))}</td>
-                                <td>{user.profession.name}</td>
-                                <td>{user.completedMeetings}</td>
-                                <td>{user.rate} /5</td>
-                                <td><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>delete</button></td>
-                            </tr>
-                        ))}
+                        {props.users.map( user => (
+                                < User 
+                                    {...user}
+                                    key={user._id}
+                                    onDelete={props.onDelete}
+                                    onBookmark={props.onBookmark} 
+                                />))}
                     </tbody>
                 </table> : ''
             }    
         </>
     );
-};
+}
 
 export default Users;
