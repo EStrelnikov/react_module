dbfa5ef9e05cfd6891d32171c3e13b6b9108fcc7
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2022-06-08 00:37:51 +0300

    task_2.14.1

--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Quality from "./quality";
+    import PropTypes from "prop-types";
+    import Quality from "./quality";
+import { useQualities } from "./../../../hooks/useQulities";
 
 const QualitiesList = ({ qualities }) => {
-    return (
-        <>
-            {qualities.map((qual) => (
-                <Quality key={qual._id} {...qual} />
-            ))}
-        </>
-    );
+    const { isLoading, getQuality } = useQualities();
+    if (!isLoading) {
+        return (
+            <>
+                {qualities.map((id) => {
+                    const qual = getQuality(id);
+                    return <Quality key={qual._id} {...qual} />;
+                })}
+            </>
+        );
+    } else return "Loading...";
 };
 
 QualitiesList.propTypes = {
