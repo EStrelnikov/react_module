e5257079b306d93fe37963a214848c14779e4ddc
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2022-06-12 16:06:07 +0300

    task_2.16.1

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -6,6 +6,14 @@ const userService = {
     get: async () => {
         const { data } = await httpService.get(userEndpoint);
         return data;
+    },
+    create: async (payload) => {
+        const { data } = await httpService.put(
+            userEndpoint + payload._id,
+            payload
+        );
+        console.log(data);
+        return data;
     }
 };
 export default userService;
